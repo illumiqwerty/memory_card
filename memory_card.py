9b2e06481ac1fec12f93738292bf2d9ac4ea9cb8
illumiqwerty <118364333+illumiqwerty@users.noreply.github.com>
2023-05-24 20:08:24 +0500

    Update memory_card.py
    v2

--- memory_card.py
+++ memory_card.py
@@ -60,10 +60,88 @@ ans_v_line.addWidget(result, alignment=(Qt.AlignHCenter | Qt.AlignVCenter ))
 ans_v_line.addWidget(correct, alignment=Qt.AlignCenter)
 AnsGroupBox.setLayout(ans_v_line)
 
-#######################################
+
 ButtonGroup = QButtonGroup()
 ButtonGroup.addButton(r_btn1)
 ButtonGroup.addButton(r_btn2)
 ButtonGroup.addButton(r_btn3)
 ButtonGroup.addButton(r_btn4)
-#######################################
+
+
+h_line1 = QHBoxLayout()
+h_line2 = QHBoxLayout()
+h_line3 = QHBoxLayout()
+main_v_line = QVBoxLayout()
+
+h_line1.addWidget(question, alignment=(Qt.AlignHCenter  |  Qt.AlignVCenter))
+h_line2.addWidget(RadioGroupBox)
+h_line2.addWidget(AnsGroupBox)
+AnsGroupBox.hide()
+h_line3.addWidget(btn)
+
+main_v_line.addLayout(h_line1)
+main_v_line.addLayout(h_line2)
+main_v_line.addLayout(h_line3)
+
+window.setLayout(main_v_line)
+
+
+def show_answer():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn.setText('Следующий вопрос')
+
+def show_question():
+    ButtonGroup.setExclusive(False)
+    r_btn1.setChecked(False)
+    r_btn2.setChecked(False)
+    r_btn3.setChecked(False)
+    r_btn4.setChecked(False)
+    ButtonGroup.setExclusive(True)
+
+    AnsGroupBox.hide()
+    RadioGroupBox.show()
+    btn.setText('Ответить')
+
+def next_question():
+    window.cur_question += 1
+    if window.cur_question == len(question_list):
+        window.cur_question = 0 
+    q = question_list[window.cur_question]
+    ask(q)
+
+
+def test():
+   if btn.text() == 'Ответить':
+       check_answer()
+   else:
+       next_question()
+answers = [r_btn1, r_btn2, r_btn3, r_btn4]
+
+def ask(q):
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    question.setText(q.quest)
+    correct.setText(q.right_answer)
+    show_question()
+
+def show_correct(res):
+    result.setText(res)
+    show_answer()
+
+def check_answer():
+    if answers[0].isChecked():
+        show_correct('Правильно!')
+    else:
+        show_correct('Не правильно!')
+
+
+btn.clicked.connect(test)
+window.cur_question = -1
+next_question()
+window.show()
+app.exec_()
+
